e29eead213664d1b840d765319b9dd570ad624fa
shivakumar323 <shivanavodaya007@gmail.com>
2022-04-10 21:37:22 +0530

    revserting back because the optimized code is not giving the expected result

--- Dec10/problem2.js
+++ Dec10/problem2.js
@@ -1,4 +1,4 @@
-// movement of a camel in chess board
+// movfement of a camel in chess board
 /*
 given a n * n matrix find out the position where a camel can travel from a given position (i, j)
 */
@@ -6,39 +6,50 @@ n = 4
 A = [];
 i = 2;
 j = 2;
-function f1(x, y) { // function to move camel to bottom right and bottom left directions
+function f1(x, y) { // function to move camel to bottom right direction 
     if(x >= n || y >= n || x < 0 || y < 0) {
-        x = i;
-        y = j;
         return;
     }
-    
     A.push(x, y);
     A.push(' ');
-    
-    f1(x + 1, y + 1);  // bottom right
-    f1(x + 1, y - 1);  // bottom left
-    //for some reason below two functions calls are resulting in maximum call stack size exceeded so gonna handle using f2 function
-    //f1(x - 1, y + 1); // top right
-    //f1(x - 1, y - 1); // top left
+
+    f1(x + 1, y + 1);
 }
-function f2(x, y) {  // function to move camel towards top right and top left directions
+function f2(x, y) {  // function to move camel towards bottom left
     if(x >= n || y >= n || x < 0 || y < 0) {
-        x = i;
-        y = j;
         return;
     }
-    if(x != i && j != y) {  // to avoid duplicates in array A
+    if(x != i && j != y) {
         A.push(x, y);
         A.push(' ');
     }
-    f2(x - 1, y + 1);  // top right
-    f2(x - 1, y - 1);  // top left
+    f2(x + 1, y - 1);
+}
+
+function f3(x, y) { // function to move camel towards top right direction
+    if(x >= n || y >= n || x < 0 || y < 0) {
+        return;
+    }
+    if(x != i && j != y) {
+        A.push(x, y);
+        A.push(' ');
+    }
+    f3(x - 1, y + 1);
+}
+
+function f4(x, y) {  // function to move camel towards top left direction
+    if(x >= n || y >= n || x < 0 || y < 0) {
+        return;
+    }
+    if(x != i && j != y) {
+        A.push(x, y);
+        A.push(' ');
+    }
+    f4(x - 1, y - 1);
 }
 
 f1(i, j);
 f2(i, j);
+f3(i, j);
+f4(i, j);
 console.log(A);
-
-// Note: this is the optimized version of https://github.com/shivakumar323/algos/commit/459332a1e9c205feb782e64135a0cc9906a918c8
-
--- Dec3/test.js
+++ Dec3/test.js
@@ -1,53 +1,26 @@
-let x1 = 7; 
-let y1 = 7;
+n = 3;
+A = [];
+i = 3;
+j = 0;
 
-let x = 3; 
-let y = 4;
-let y2 = y;
-let y3 = y;
-y4 = y;
 
-let A = [];
-A.push(x);
-A.push(y);
-A.push(' ');
-for(let j = x - 1; j >= 0; j--) {
-    
-    y3 -= 1;
-    if(y3 >= 0) {
-        A.push(j);
-        A.push(y3);
+function f2(x, y) {  // function to move camel towards top right and top left directions
+    if(x > n || y > n || x < 0 || y < 0) {
+        x = i;
+        y = j;
+        return;
+    }
+    if(x != i && y != j) {  // to avoid duplicates in array A
+        A.push(x, y);
         A.push(' ');
     }
-    
-    
-    y4 += 1;
-    if(y4 <= x1) {
-        A.push(j);
-        A.push(y4);
-        A.push(' ');
+    f2(x - 1, y - 1);  // top left
+    //f2(x - 1, y + 1);  // top right
+    if((x == i) && (y == j)) {
+        f2(x - 1, y + 1);
     }
     
 }
 
-for(let i = x + 1; i <= x1; i++) {
-
-    y += 1;
-    if(y <= x1) {
-        A.push(i);
-        A.push(y);
-        A.push(' ');
-    }
-    
-    
-    y2 -= 1;
-    if(y2 >= 0) {
-        A.push(i);
-        A.push(y2);
-        A.push(' ');
-    }
-    
-
-}
-
+f2(i, j);
 console.log(A);
